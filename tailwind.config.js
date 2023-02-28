958e361fd5e370928f0760ec08d06109ef79dd0a
Arquidex ~ Arki <88553657+carameltora@users.noreply.github.com>
2023-02-28 13:19:58 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+        '2': 'repeat(8, minmax(0, 1fr))',
+
+        // Complex site-specific row configuration
+        'layout': '100px minmax(100px, 1fr) 100px',
+      }
+    },
+  },
+  plugins: [],
+}
